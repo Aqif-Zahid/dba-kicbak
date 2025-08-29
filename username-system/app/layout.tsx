4003b3af6e55ce9a191354c1ee6f1d4b3f5c7eb6
arlenritchie <151496980+arlenritchie@users.noreply.github.com>
2025-08-29 17:34:44 -0500

    Update layout.tsx
    Step: Wrap your app with ClerkProvider

--- username-system/app/layout.tsx
+++ username-system/app/layout.tsx
@@ -1,26 +1,17 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+// app/layout.tsx
+import { ClerkProvider } from "@clerk/nextjs";
 
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+export const metadata = {
+  title: "kicbak",
+  description: "kicbak app",
+};
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
